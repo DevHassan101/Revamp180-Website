5cb6a45da1c2e02523ad4f43ba3bfd38c986dec9
DevHassan101 <hassanshaikh311203@gmail.com>
2026-06-04 18:10:45 +0500

    favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${poppins.variable} h-full antialiased`}>
+      <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon_io/apple-touch-icon.png"></link>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon_io/favicon-32x32.png"></link>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon_io/favicon-16x16.png"></link>
+        <link rel="manifest" href="/favicon_io/site.webmanifest"></link>
+      </head>
       <body className="min-h-full flex flex-col font-[family-name:var(--font-poppins)]">
         <CursorTrail />
         <Navbar />
